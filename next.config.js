0c62ee938588f01866e5788767180f638b040008
aryangupta701 <garyan447@gmail.com>
2023-06-25 23:32:39 +0530

    added secret

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     env : {
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-        MONGODB_URI: process.env.MONGODB_URI
+        MONGODB_URI: process.env.MONGODB_URI,
+        secret: process.env.SECRET
     }   
 }
 
